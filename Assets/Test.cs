2045d9bbad98f168ca47d522d0174e258c60da96
AMan <ManXiaoMan7239@gmail.com>
2023-08-15 06:11:18 +0800

    第二次提交

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    private string str = "asd";
+    private string str = "First";
+    private string ABC = "ABC";
     // Start is called before the first frame update
     void Start()
     {
